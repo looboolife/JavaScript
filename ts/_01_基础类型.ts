66fe099695997f7abad7a373ac5e7d28788001ed
looboolife <looboolife@163.com>
2021-04-26 01:20:38 +0800

    first commit

---
+++ ts/_01_基础类型.ts
@@ -0,0 +1,23 @@
+/**
+ * boolean
+ */
+let isDone: boolean = false;
+/**
+ * number
+ */
+let decLiteral: number = 6;
+let hexliteral: number = 0xf00d;
+//二进制
+let binaryLiteral: number = 0b1010;
+//八进制
+let octalLiteral: number = 0o744;
+//es2020 才可以
+// let bigLiteral:bigint = 100n;
+/**
+ * string
+ */
+let name: string = "bob";
+name = "smith";
+
+
+export {}
